93563c98906aa2d0e8ab9158056561ab5fa9ce5a
nagassoway <nagassoway@gmail.com>
2020-03-12 20:49:18 -0400

    update

---
+++ app/controllers/searching_controller.rb
@@ -0,0 +1,5 @@
+SearchingController < ApplicationController
+    def render_searching
+        render searching_index
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   resources :reviews
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'searching#render_searching'
 end
