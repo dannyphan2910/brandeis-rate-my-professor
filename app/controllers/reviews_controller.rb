e7f662713a314c0626223ef08e01492828f7f7fa
nossu3751 <rhosungpark@gmail.com>
2020-03-23 21:01:00 -0400

    edited some css of bootstrap form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -90,6 +90,11 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def autocomplete
+    results = AutocompleteSearchService.new(params[:q]).callbacks
+    render json: results
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_review
@@ -105,4 +110,28 @@ class ReviewsController < ApplicationController
         :professor_id,
       )
     end
+end
+
+class AutocompleteSearchService
+  include HTTParty
+  base_uri "https://api.github.com/"
+
+  def initialize(term)
+    @term = term
+  end
+
+  def call
+    { users: users, skills: skills }
+  end
+
+  private
+
+  def users
+    response = self.class.get("/search/users", query: { q: @term })
+    response["items"].map { |u| u["login"] }.take(5)
+  end
+
+  def skills
+    Skill.find_by_name(@term).map(&:name).take(5)
+  end
 end
