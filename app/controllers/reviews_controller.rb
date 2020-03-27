68185d747fee2dcbcd7d28d8f466de02932b8fa9
nossu3751 <rhosungpark@gmail.com>
2020-03-27 01:35:35 -0400

    submodule

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -112,26 +112,3 @@ class ReviewsController < ApplicationController
     end
 end
 
-class AutocompleteSearchService
-  include HTTParty
-  base_uri "https://api.github.com/"
-
-  def initialize(term)
-    @term = term
-  end
-
-  def call
-    { users: users, skills: skills }
-  end
-
-  private
-
-  def users
-    response = self.class.get("/search/users", query: { q: @term })
-    response["items"].map { |u| u["login"] }.take(5)
-  end
-
-  def skills
-    Skill.find_by_name(@term).map(&:name).take(5)
-  end
-end
