957b55cada055719cbbee0175e07947936a46aaa
Danny Phan <phanhaidang29102000@gmail.com>
2020-05-03 20:42:27 -0400

    some fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,19 +25,6 @@ require 'open-uri'
 
 url = 'https://www.cs.brandeis.edu/~prakhar/dataset.json'
 
-# course_file = File.read('public/json/course.json')
-# instructor_file = File.read('public/json/instructor.json')
-# subject_file = File.read('public/json/subject.json')
-
-# course_hash = JSON.parse(course_file)
-# instructor_hash = JSON.parse(instructor_file)
-# subject_hash = JSON.parse(subject_file)
-
-# subjects = []
-# subject_hash.each do |subject|
-#     subjects.push(subject['name'])
-# end
-
 1000.times do 
     pass = Faker::Internet.password(min_length: 10, max_length: 20)
     u = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: pass, password_confirmation: pass, is_admin: false)
@@ -103,54 +90,6 @@ sections.each_value do |section|
     semester_full = term["name"].split(" ", 2)
     c = Course.create(general_course_id: gc.id, professor_id: prof.id, semester: semester_full[0], year: semester_full[1].to_i)
 end
-
-# instructor_hash.each do |instructor|
-#     p = Professor.create(prof_first_name: instructor['first'], prof_last_name: instructor['last'], prof_email: instructor['email'], dept_name: subjects.sample, prof_info: Faker::Job.title)
-# end
-
-# assign_hash = {
-#     "MATH" => "Mathematics",
-#     "COSI" => "Computer Science",
-#     "ECON" => "Economics",
-#     "ENG" => "English",
-#     "ANTH" => "Anthropology",
-#     "POL" => "Politics",
-#     "IGS" => "International and Global Studies",
-#     "PSYC" => "Psychology",
-#     "BUS" => ["Economics", "Business", "International Business School"],
-#     "FIN" => ["Business", "International Business School"],
-#     "PHYS" => ["Physics", "Biophysics and Structural Biology", "Biochemistry and Biophysics"],
-#     "CHEM" => ["Biochemistry and Biophysics", "Biochemistry", "Chemistry"],
-#     "BCHM" => "Biochemistry and Biophysics",
-#     "BIO" => ["Molecular and Cell Biology", "Quantitative Biology", "Biophysics and Structural Biology", "Biochemistry and Biophysics", "Biochemistry"],
-#     "NEJS" => ["Hornstein Jewish Professional Leadership Program", "Islamic and Middle Eastern Studies"],
-#     "KOR" => "Korean",
-#     "JAPN" => "Japanese",
-#     "RUS" => "Russian Studies",
-#     "MUS" => "Music",
-#     "THA" => "Theater Arts",
-#     "HBRW" => "Hebrew",
-#     "FREN" => "French and Francophone Studies",
-#     "ARBC" => "Arabic Language and Literature", 
-#     "HINDI" => "Hindi",
-#     "LAT" => "Latin",
-#     "GER" => "German Studies",
-#     "CHIN" => "Chinese",
-#     "GRK" => "Greek",
-#     "ITAL" => "Italian Studies"
-# }
-
-
-# course_hash.each do |course| 
-#     course_code = course['code'].split(' ')[0]
-#     if assign_hash.keys.include? course_code
-#         gen_c = GeneralCourse.create(course_code: course['code'], course_title: course['name'], course_description: course['description'])
-#         5.times do 
-#             p_id = Professor.where(dept_name: assign_hash[course_code]).sample.id
-#             c = Course.create(professor_id: p_id, semester: ['Fall', 'Spring'].sample, year: Faker::Number.between(from: 2005, to: 2020), general_course_id: gen_c.id)
-#         end
-#     end
-# end
 
 i = 0
 10000.times do
