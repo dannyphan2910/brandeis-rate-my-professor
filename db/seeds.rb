7e8401ff772b77d8a9cb2f69ea66aa98e6d7fb2c
Danny Phan <phanhaidang29102000@gmail.com>
2020-05-19 16:59:35 +0700

    update seed file to scale down

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,11 +25,6 @@ require 'open-uri'
 
 url = 'https://www.cs.brandeis.edu/~prakhar/dataset.json'
 
-1000.times do 
-    pass = Faker::Internet.password(min_length: 10, max_length: 20)
-    u = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: pass, password_confirmation: pass, is_admin: false)
-end
-
 courses = {}
 sections = {}
 terms = {}
@@ -56,6 +51,20 @@ end
 
 puts 'DONE READ JSON'
 
+# MODIFY THESE LINES TO SEED FULL DATA (> 500k data)
+limit_users = 10 # default (if commented): 1000 users (excluding 1 super-admin)
+limit_course_prof = 1500 # comment this line to load full dataset
+limit_review = 1500 # default (if commented): 10000 reviews
+limit_votes = 2 # default (if commented): 0 -> 50 rates up/down per review
+###
+
+num_users = limit_users || 1000
+num_users.times do 
+    pass = Faker::Internet.password(min_length: 10, max_length: 20)
+    u = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: pass, password_confirmation: pass, is_admin: false)
+end
+
+count_course_prof = 0
 sections.each_value do |section|
     course_id = section["course"]
     course = courses[course_id]
@@ -89,25 +98,32 @@ sections.each_value do |section|
     term = terms[term_id]
     semester_full = term["name"].split(" ", 2)
     c = Course.create(general_course_id: gc.id, professor_id: prof.id, semester: semester_full[0], year: semester_full[1].to_i)
+
+    count_course_prof += 1
+    if limit_course_prof && limit_course_prof <= count_course_prof
+        break
+    end
 end
 
-i = 0
-10000.times do
+count_review = 0
+num_reviews = limit_review || 10000
+max_rates = limit_votes || 50
+num_reviews.times do
     c_id = Course.pluck(:id).sample
     r = Review.create(user_id: User.pluck(:id).sample, course_id: c_id, professor_id: Course.find(c_id).professor.id, title: Faker::Movie.quote)
     course_r = CourseRating.create(review_id: r.id, cat1: Faker::Number.between(from: 1, to: 5), cat2: Faker::Number.between(from: 1, to: 5), cat3: Faker::Number.between(from: 1, to: 5), cat4: Faker::Number.between(from: 1, to: 5), cat5: Faker::Number.between(from: 1, to: 5), content: Faker::Quote.famous_last_words)
     prof_r = ProfessorRating.create(review_id: r.id, cat1: Faker::Number.between(from: 1, to: 5), cat2: Faker::Number.between(from: 1, to: 5), cat3: Faker::Number.between(from: 1, to: 5), cat4: Faker::Number.between(from: 1, to: 5), cat5: Faker::Number.between(from: 1, to: 5), strength: Faker::Quote.famous_last_words, improvement: Faker::Quote.famous_last_words)
 
-    rand(0..50).times do
+    rand(0..max_rates).times do
         vote_up = RateUp.create(user_id: User.pluck(:id).sample, review_id: r.id)
     end
 
-    rand(0..50).times do
+    rand(0..max_rates).times do
         vote_down = RateDown.create(user_id: User.pluck(:id).sample, review_id: r.id)
     end
 
-    puts i
-    i+=1
+    count_review += 1
+    puts count_review
 end
 
 admin = User.create(first_name: "Admin", last_name: "Brandeis", email: "admin@brandeis.edu", password: "brandeis_admin", password_confirmation: "brandeis_admin", is_admin: true)
